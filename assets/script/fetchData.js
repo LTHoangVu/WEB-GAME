9f94bdaa5957bdf7b0aca9e19a9d79fc29d5d44a
Truong Le <21522732@gm.uit.edu.vn>
2023-11-20 12:48:51 +0700

    Add sale off style if item is sale off

--- assets/script/fetchData.js
+++ assets/script/fetchData.js
@@ -1,32 +1,3 @@
-const token = localStorage.getItem("token");
-
-// Fetch user information
-async function fetchUserInfo() {
-  if (token) {
-    try {
-      const response = await fetch("http://localhost:8080/auth/user", {
-        headers: {
-          Authorization: `Bearer ${token}`,
-          "Content-Type": "application/json",
-        },
-      });
-      if (!response.ok) {
-        const error = new Error("Fetching user failed");
-        error.code = response.status;
-        error.info = await response.json();
-        throw error;
-      }
-      const resData = await response.json();
-      return resData;
-    } catch (error) {
-      console.log(error);
-    }
-  } else {
-    return false;
-    // window.location.assign("./login.html");
-  }
-}
-
 // Fetch all products
 async function fetchProductsData() {
   try {
@@ -44,4 +15,4 @@ async function fetchProductsData() {
   }
 }
 
-export { fetchProductsData, fetchUserInfo };
+export { fetchProductsData };
