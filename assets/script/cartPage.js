7722481635193abd13cbf4684877adb319dbb99e
ntritin62 <21522677@gm.uit.edu.vn>
2023-11-26 17:40:34 +0700

    fix bug

--- assets/script/cartPage.js
+++ assets/script/cartPage.js
@@ -1,20 +1,19 @@
-import { fetchUserCart } from "./fetchCart.js";
+import { fetchUserCart } from './fetchCart.js';
 
 async function loadDataOnCart() {
-    const data = await fetchUserCart();
-    const cartData = data.cart;
-    console.log(data);
-    loadItemsOnCart(cartData);
-    deleteItemsOnCart(cartData);
-    
+  const data = await fetchUserCart();
+  const cartData = data.cart;
+  console.log(data);
+  loadItemsOnCart(cartData);
+  deleteItemsOnCart(cartData);
 }
 
 async function loadItemsOnCart(cart) {
-    const cartListContainer = document.getElementById("cart_item_list");
-    const cartTotalPrice = document.getElementById("cart_estimated_total");
-    let totalPrice = 0;
-    for (let item of cart) {
-        const itemHtmlContent =  `
+  const cartListContainer = document.getElementById('cart_item_list');
+  const cartTotalPrice = document.getElementById('cart_estimated_total');
+  let totalPrice = 0;
+  for (let item of cart) {
+    const itemHtmlContent = `
         <div class="cart_item">
             <div class="cart_item_price">
                 <div class="price">${item.price}₫</div>
@@ -32,55 +31,59 @@ async function loadItemsOnCart(cart) {
                 <a href="http://localhost:8080/products/${item._id}">${item.title}</a><br>
             </div>
         </div>`;
-        cartListContainer.innerHTML += itemHtmlContent;
-        totalPrice += item.price;
-    }
-    cartTotalPrice.innerHTML = totalPrice + "₫";
+    cartListContainer.innerHTML += itemHtmlContent;
+    totalPrice += item.price;
+  }
+  cartTotalPrice.innerHTML = totalPrice + '₫';
 }
 
 async function deleteDataOnCart(idGame) {
-    const token = localStorage.getItem('token');
-    if (token) {
-        try {
-            const response = await fetch('http://localhost:8080/shop/delete-in-cart', { 
-            method: 'DELETE',
-            headers: {
-                Authorization: `Bearer ${token}`,
-                'Content-Type': 'application/json',
-            },
-            body: JSON.stringify({gameId: idGame})
-            });
-            if (!response.ok) {
-                const error = new Error('Deleting game to cart failed');
-                error.code = response.status;
-                error.info = await response.json();
-                throw error;
-            }
-            const resData = await response.json();
-            return resData;
-        } catch (error) {
-            console.log(error);
+  const token = localStorage.getItem('token');
+  console.log(idGame);
+  if (token) {
+    try {
+      const response = await fetch(
+        'http://localhost:8080/shop/delete-in-cart',
+        {
+          method: 'DELETE',
+          headers: {
+            Authorization: `Bearer ${token}`,
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify({ gameId: idGame }),
         }
-    } else {
-        window.location.assign('./login.html');
+      );
+      if (!response.ok) {
+        const error = new Error('Deleting game to cart failed');
+        error.code = response.status;
+        error.info = await response.json();
+        throw error;
+      }
+      const resData = await response.json();
+      return resData;
+    } catch (error) {
+      console.log(error);
     }
+  } else {
+    window.location.assign('./login.html');
+  }
 }
 
 async function deleteItemsOnCart(cart) {
-    const deleteBtn = document.getElementsByClassName("remove_each");
-    console.log(deleteBtn);
-    for (let i = 0; i < deleteBtn.length; i++) {   
-        let button = deleteBtn[i];
-        button.onclick = async function(event) {
-            const buttonClicked = event.target;
-            console.log(cart[i]._id);
-            deleteDataOnCart(cart[i]._id);
-            buttonClicked.parentElement.parentElement.remove();
-            /*location.reload();*/
-            return false;
-        }
-    }
+  const deleteBtn = document.getElementsByClassName('remove_each');
+  console.log(deleteBtn);
+  for (let i = 0; i < deleteBtn.length; i++) {
+    let button = deleteBtn[i];
+    console.log(button);
+    button.onclick = async function (event) {
+      const buttonClicked = event.target;
+      console.log(cart[i]._id);
+      await deleteDataOnCart(cart[i]._id);
+      buttonClicked.parentElement.parentElement.remove();
+      /*location.reload();*/
+      return false;
+    };
+  }
 }
-
 
 export { loadDataOnCart };
